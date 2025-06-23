0757d9011b21d5ae26a9545b5971b69988ddc572
ymm <ymm@bowang.com>
2025-06-23 12:50:44 +0800

    fix: cli

---
+++ packages/cli/src/cliAppInit.ts
@@ -0,0 +1,92 @@
+import { AppInit, inject, Injector, OnInit, Type } from "@decopro/core";
+import commander from "commander";
+import {
+    ACTION_TOKEN,
+    ARGUMENT_TOKEN,
+    COMMANDER_TOKEN,
+    OPTION_TOKEN
+} from "@decopro/commander";
+import { TestCommand } from "./testCommand";
+
+@AppInit({
+    deps: []
+})
+export class CliAppInit implements OnInit {
+    private readonly commanders: Type<any>[] = [
+        TestCommand
+    ];
+    constructor(@inject(Injector) private injector: Injector) { }
+    async onInit(): Promise<void> {
+        const injector = this.injector;
+        const commands = injector.getAll(COMMANDER_TOKEN);
+        commands.map((command) => {
+            const c = new commander.Command();
+            const options = command.options;
+            if (options.name) c.name(options.name);
+            if (options.alias) c.alias(options.alias);
+            if (options.description) c.description(options.description);
+            if (options.summary) c.summary(options.summary);
+            const _arguments = injector
+                .getAll(ARGUMENT_TOKEN)
+                .filter((it) => it.target === command.target);
+            _arguments.map((arg) => {
+                const options = arg.options;
+                const ins = new commander.Argument(
+                    options.name,
+                    options.description
+                );
+                if (options.defaultValue)
+                    ins.defaultValue = options.defaultValue;
+                c.addArgument(ins);
+            });
+            const _options = injector
+                .getAll(OPTION_TOKEN)
+                .filter((it) => it.target === command.target);
+            _options.map((arg) => {
+                const options = arg.options;
+                const ins = new commander.Option(
+                    options.flags,
+                    options.description
+                );
+                if (options.zod) {
+                    ins.argParser((value, previous) => {
+                        return options.zod.parse(value);
+                    });
+                }
+                c.addOption(ins);
+            });
+            c.action(async (...args: any[]) => {
+                // 最后一个是command对象
+                args.pop();
+                // 倒数第二个是options
+                const options = args.pop();
+                // 其余的是argument
+                const _actions = injector
+                    .getAll(ACTION_TOKEN)
+                    .filter((it) => it.target === command.target);
+                const instance = injector.get(command.target);
+                const actions = _actions.map((action) => {
+                    const method = Reflect.get(instance, action.property);
+                    if (typeof method === "function") {
+                        return method.bind(instance);
+                    }
+                    throw new Error(
+                        `${command.target.name
+                        }.${action.property.toString()} is not found`
+                    );
+                });
+                _options.map((option) => {
+                    const value = Reflect.get(options, option.property);
+                    Reflect.set(instance, option.property, value);
+                });
+                _arguments.map((arg, idx) => {
+                    const value = Reflect.get(args, idx);
+                    Reflect.set(instance, arg.property, value);
+                });
+                await Promise.all(actions.map((action) => action(...args)));
+            });
+            commander.program.addCommand(c);
+        });
+        commander.program.parse(process.argv);
+    }
+}
---
+++ packages/cli/src/testCommand.ts
@@ -0,0 +1,23 @@
+import { Action, Commander } from "@decopro/commander";
+import { Injector, inject } from "@decopro/core";
+import { runTest } from "@decopro/test";
+@Commander({
+    name: `test`,
+    description: `测试`
+})
+export class TestCommand {
+    constructor(@inject(Injector) private injector: Injector) { }
+    @Action({})
+    async test() {
+        const errors = await runTest(this.injector);
+        const errMsg = errors
+            .filter((it) => !it.result)
+            .map((it) => {
+                return `${it.target.name}.${it.options.description || it.property.toString()
+                    }: Failed`;
+            })
+            .join("\n");
+        console.log(`-------测试结果-----`)
+        console.log(errMsg);
+    }
+}
