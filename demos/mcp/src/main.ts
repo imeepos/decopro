c24c4cb11b73ed010bcd96793433ed0d142af79f
ymm <ymm@bowang.com>
2025-06-23 14:13:06 +0800

    fix

--- demos/mcp/src/main.ts
+++ demos/mcp/src/main.ts
@@ -1,7 +1,6 @@
 import "reflect-metadata";
 import { bootstrap } from "@decopro/core";
-import { CommanderAppInit } from "./commanders";
 import { McpAppInit } from "./mcp";
 import { McpTest } from "./tests";
 
-bootstrap([CommanderAppInit, McpAppInit, McpTest]);
+bootstrap([McpAppInit, McpTest]);
