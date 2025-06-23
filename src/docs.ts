9b95b3b3ee1261b243e15eb5a1882c766ab80766
ymm <ymm@bowang.com>
2025-06-23 11:20:59 +0800

    feature: add docs

---
+++ src/docs.ts
@@ -0,0 +1,6 @@
+import { generateTSKnowledges } from '@decopro/docs'
+import { join } from 'path'
+
+const root = process.cwd()
+const knowledges = generateTSKnowledges(join(root, 'packages/ast'))
+
